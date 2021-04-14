e9aaa9793c7e726fc5cfae8ff06cc9b2a15b73d6
rz4life <rzalagbada@Abdulrazaqs-Air.cable.rcn.com>
2021-04-14 16:25:05 -0500

    associations table

---
+++ models/business.js
@@ -0,0 +1,31 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class business extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      models.business.belongsTo(models.type)
+      models.business.hasMany(models.review)
+      models.business.belongsTo(models.user)
+    }
+  };
+  business.init({
+    name: DataTypes.TEXT,
+    address: DataTypes.TEXT,
+    type: DataTypes.TEXT,
+    description: DataTypes.TEXT,
+    typeId:DataTypes.INTEGER,
+    userId:DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'business',
+  });
+  return business;
+};
